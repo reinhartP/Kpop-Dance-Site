672e94a78a91e4fb3cb759a6ed36d5c5f25f9db0
Reinhart Paghunie <rp@Reinharts-MacBook-Pro.local>
2019-07-23 16:44:34 -0700

    list songs in a grid and open a modal with the song

---
+++ src/components/GridList.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Modal, Grid, Container } from 'semantic-ui-react';
+import './GridList.css';
+function ArtistList(props) {
+    if (props.artists.length > 0) {
+        let songs = [];
+        props.artists.forEach((artist) => {
+            artist.songs.forEach(song => {
+                songs.push({
+                    artist: artist.artist,
+                    title: song.title,
+                    thumbnail: song.thumbnail,
+                    id: song.id,
+                })
+            })
+        })
+        return (
+            <div className="grid">
+                {songs.map((song) => {
+                    return(
+                        <div style={{width: "210px"}}>
+                            <a href={`player/${song.id}`}>
+                            <img src={song.thumbnail} style={{width: '100%'}} ></img>
+                            </a>
+                            <a style={{color: 'white'}} onClick={() => props.status(true, song.id)}>
+                                {song.title.length > 45 ? song.title.slice(0,45).concat('...') : song.title}
+                            </a>
+                        </div>
+                                
+                    )
+                })}
+
+            </div>
+        )
+    }
+    return <div />;
+}
+class GridList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            artists: this.props.artists,
+            open: false,
+            id: '',
+        }
+        this.receiveStatus = this.receiveStatus.bind(this);
+        this.close = this.close.bind(this);
+    }
+    receiveStatus(open, id) {
+        this.setState({
+            open,
+            id
+        })
+    }
+    close = () => this.setState({open: false});
+    componentDidMount() {
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.artists !== this.props.artists) {
+            this.setState({
+                artists: this.props.artists
+            })
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Modal size="fullscreen" dimmer="blurring" closeIcon open={this.state.open} onClose={this.close}>
+                    <Modal.Description>
+                        <iframe
+                            style={{
+                            width: '100%',
+                            height: '720px',
+                        }}
+                        title="Mirrored YouTube Video"
+                        src={`player/${
+                            this.state.id
+                        }`}
+                    />
+                </Modal.Description>
+            </Modal>
+            <ArtistList artists={this.state.artists} status={this.receiveStatus}></ArtistList>
+            </div>
+        )
+    }
+}
+
+export default GridList;
