844a973787f4481f929a22829f1d18825115052b
reinhartP <reinhartjp@gmail.com>
2019-07-24 09:10:22 -0700

    changed the way the modal/iframe is loaded

--- src/components/GridList.js
+++ src/components/GridList.js
@@ -4,34 +4,44 @@ import './GridList.css';
 function ArtistList(props) {
     if (props.artists.length > 0) {
         let songs = [];
-        props.artists.forEach((artist) => {
+        props.artists.forEach(artist => {
             artist.songs.forEach(song => {
                 songs.push({
                     artist: artist.artist,
                     title: song.title,
                     thumbnail: song.thumbnail,
                     id: song.id,
-                })
-            })
-        })
+                });
+            });
+        });
         return (
             <div className="grid">
-                {songs.map((song) => {
-                    return(
-                        <div style={{width: "210px"}}>
-                            <a href={`player/${song.id}`}>
-                            <img src={song.thumbnail} style={{width: '100%'}} ></img>
+                {songs.map(song => {
+                    return (
+                        <div style={{ width: '210px' }}>
+                            <a onClick={() => props.status(true, song.id)}>
+                                <img
+                                    src={song.thumbnail}
+                                    style={{ width: '100%', cursor: 'pointer' }}
+                                />
                             </a>
-                            <a style={{color: 'white'}} onClick={() => props.status(true, song.id)}>
-                                {song.title.length > 45 ? song.title.slice(0,45).concat('...') : song.title}
+                            <a
+                                style={{
+                                    color: 'white',
+                                    cursor: 'pointer',
+                                    fontSize: '0.9em',
+                                }}
+                                onClick={() => props.status(true, song.id)}
+                            >
+                                {song.title.length > 45
+                                    ? song.title.slice(0, 45).concat('...')
+                                    : song.title}
                             </a>
                         </div>
-                                
-                    )
+                    );
                 })}
-
             </div>
-        )
+        );
     }
     return <div />;
 }
@@ -42,46 +52,53 @@ class GridList extends Component {
             artists: this.props.artists,
             open: false,
             id: '',
-        }
+        };
         this.receiveStatus = this.receiveStatus.bind(this);
         this.close = this.close.bind(this);
     }
     receiveStatus(open, id) {
         this.setState({
             open,
-            id
-        })
+            id,
+        });
     }
-    close = () => this.setState({open: false});
-    componentDidMount() {
-    }
+    close = () => this.setState({ open: false });
+    componentDidMount() {}
     componentDidUpdate(prevProps, prevState) {
-        if(prevProps.artists !== this.props.artists) {
+        if (prevProps.artists !== this.props.artists) {
             this.setState({
-                artists: this.props.artists
-            })
+                artists: this.props.artists,
+            });
         }
     }
     render() {
         return (
             <div>
-                <Modal size="fullscreen" dimmer="blurring" closeIcon open={this.state.open} onClose={this.close}>
+                <Modal
+                    size="large"
+                    dimmer="blurring"
+                    centered={false}
+                    closeIcon
+                    open={this.state.open}
+                    onClose={this.close}
+                >
                     <Modal.Description>
                         <iframe
                             style={{
-                            width: '100%',
-                            height: '720px',
-                        }}
-                        title="Mirrored YouTube Video"
-                        src={`player/${
-                            this.state.id
-                        }`}
-                    />
-                </Modal.Description>
-            </Modal>
-            <ArtistList artists={this.state.artists} status={this.receiveStatus}></ArtistList>
+                                width: '100%',
+                                height: '720px',
+                            }}
+                            title="Mirrored YouTube Video"
+                            src={`player/${this.state.id}`}
+                        />
+                    </Modal.Description>
+                </Modal>
+                <ArtistList
+                    artists={this.state.artists}
+                    status={this.receiveStatus}
+                />
             </div>
-        )
+        );
     }
 }
 
