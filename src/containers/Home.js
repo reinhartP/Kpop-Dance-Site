2d1c4dc567b8b1f152b17c22c82193dbe160b977
reinhartP <reinhartjp@gmail.com>
2019-07-24 09:08:20 -0700

    sort list by artist name and add loading animation

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -14,6 +14,7 @@ class Home extends Component {
             artists: {},
             isLoading: true,
         };
+        this.sortSongs = this.sortSongs.bind(this);
     }
     async componentDidMount() {
         document.body.style.background = '#202124';
@@ -27,21 +28,39 @@ class Home extends Component {
                         artists: response.data.info,
                     });
             });
+        this.sortSongs();
+    }
+
+    sortSongs(type = 'artist asc') {
+        function compareArtist(a, b) {
+            if (a.artist > b.artist) return 1;
+            return -1;
+        }
+        if (this.state.artists.length > 0)
+            switch (type) {
+                case 'artist asc':
+                    this.setState(currentState => {
+                        currentState.artists.sort(compareArtist);
+                        return {
+                            artists: currentState.artists,
+                        };
+                    });
+                    break;
+            }
     }
     render() {
-        if(!this.state.isLoading)
-        {
-        return (
-            <div>
-                <GridList artists={this.state.artists}></GridList>
-            </div>
-        );
+        if (!this.state.isLoading) {
+            return (
+                <div style={{ paddingTop: '100px' }}>
+                    <GridList artists={this.state.artists} />
+                </div>
+            );
         }
         return (
             <div>
-                <Loader active={this.state.isLoading}/>
+                <Loader active={this.state.isLoading} />
             </div>
-        )
+        );
     }
 }
 
