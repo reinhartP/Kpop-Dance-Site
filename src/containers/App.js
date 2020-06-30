8d15b8b8860ed5b1c20ad6f415ab7b3448b447c5
reinhartP <reinhartjp@gmail.com>
2020-06-29 20:33:11 -0700

    removed admin page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import AdminPage from './Admin';
-
+import Routes from '../routes';
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <AdminPage />
+                <Routes />
             </div>
         );
     }
